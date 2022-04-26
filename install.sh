96b6f7b393fba056c6fef3ba293fb9f03f4a5e62
Daniel Seripap <683200+seripap@users.noreply.github.com>
2022-04-26 16:10:46 -0400

    Add sshb0t

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+docker run -d --restart always \
+    --name sshb0t \
+    -v ${HOME}/.ssh/authorized_keys:/root/.ssh/authorized_keys \
+    r.j3ss.co/sshb0t --user seripap --keyfile /root/.ssh/authorized_keys
+
 # echo "Linking vimrc"
 # ln -s .vimrc $HOME/.vimrc
 
