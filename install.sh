8d6a44c91f56a508765e8d785d330b83161a43e1
Daniel Seripap <683200+seripap@users.noreply.github.com>
2022-05-02 11:25:59 -0400

    remove sshb0t

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+echo "Installing VIM Plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-docker run -d --restart always \
-    --name sshb0t \
-    -v ${HOME}/.ssh/authorized_keys:/root/.ssh/authorized_keys \
-    r.j3ss.co/sshb0t --user seripap --keyfile /root/.ssh/authorized_keys
+# echo "Installing sshb0t"
+# docker run -d --restart always \
+#     --name sshb0t \
+#     -v ${HOME}/.ssh/authorized_keys:/root/.ssh/authorized_keys \
+#     r.j3ss.co/sshb0t --user seripap --keyfile /root/.ssh/authorized_keys
 
 # echo "Linking vimrc"
 # ln -s .vimrc $HOME/.vimrc
